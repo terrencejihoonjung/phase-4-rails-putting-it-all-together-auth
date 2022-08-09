ca6f37bc125432f3a6eaa9a6d62faa5b8021735e
terrencejung <terrencejung928@gmail.com>
2022-08-08 18:14:40 -0700

    commit tree cf50bc7675bc49ff6948e44660883db5e14e2cea

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+
+  before_action :authorize
+
+  private
+
+  def authorize
+    @currentuser = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @currentuser
+  end
+
+  def unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
 end
